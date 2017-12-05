4e412e237b9d1443751bb4ccaffd2271ea66763c
Johannes Schoenboeck <johannes.schoenboeck@fh-hagenberg.at>
2017-12-05 14:34:26 +0100

    added Groups

--- G2/WhatsApp/js/whatsapp.js
+++ G2/WhatsApp/js/whatsapp.js
@@ -11,15 +11,23 @@ class WhatsApp {
     loadFromJSON(){
 
         $.getJSON("json/contacts.json",(data) => {
-            console.log(this);
-           console.log(data);
            for(let person of data.persons){
                let contact = new Person(person.id,person.name,person.img,person.online);
                this.contactList.addContact(contact);
            }
 
            for(let group of data.groups){
-
+                let g = new Group(group.id,group.name, group.img);
+                for(let contactId of group.members){
+                    if(contactId!=this.userId){
+                        let c = this.contactList.getContactById(contactId);
+                        if(c){
+                            g.addContact(c);
+                            c.addGroup(g);
+                        }
+                    }
+                }
+                this.contactList.addContact(g);
            }
            this.contactList.printContactList();
         });
