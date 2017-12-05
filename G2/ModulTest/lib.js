aab0d0b4a4ad9f9f693103724a8a5754d74495b9
Johannes Schoenboeck <johannes.schoenboeck@fh-hagenberg.at>
2017-12-05 16:17:31 +0100

    added Groups

---
+++ G2/ModulTest/lib.js
@@ -0,0 +1,9 @@
+const  sqrt = Math.sqrt;
+
+function square(x){
+    return x*x;
+}
+
+export function diag(x,y){
+    return sqrt(square(x)+square(y));
+}
---
+++ G2/ModulTest/main.js
@@ -0,0 +1,4 @@
+import { diag} from './lib.js';
+
+console.log(diag(4,3));
+console.log(square(5));
