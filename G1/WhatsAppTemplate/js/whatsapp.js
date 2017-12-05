e38ab78f4d06db7e5895e4aec884dd2a6c54f096
Johannes Schoenboeck <johannes.schoenboeck@fh-hagenberg.at>
2017-12-05 10:17:51 +0100

    Some code input for next week

--- G1/WhatsAppTemplate/js/whatsapp.js
+++ G1/WhatsAppTemplate/js/whatsapp.js
@@ -17,13 +17,31 @@ class WhatsApp {
                 let contact = new Person(person.id,person.name,person.img,person.online);
                 console.log(contact);
                 this.contactList.addContact(contact);
+                this.addMessageToContact(contact,person,false);
             }
             for(let group of data.groups) {
                 let g = new Group(group.id, group.name, group.img);
                 this.contactList.addContact(g);
+                this.addMessageToContact(g,group,true);
+                for(let contactId of group.members){
+                    if(contactId !== this.personnelId){
+                        let c = this.contactList.getContactById(contactId);
+                        if(c){
+                            g.addContact(c);
+                            c.addGroup(g);
+                        }
+                    }
+                }
             }
 
             this.contactList.printContactList();
         });
+    }
+
+    addMessageToContact(contact,jsonContact,isGroupMsg){
+        for(let msg of jsonContact.messages){
+            let message = new Message(msg.text,msg.time,msg.senderId,isGroupMsg);
+            contact.addMessage(message);
+        }
     }
 }
