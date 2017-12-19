aaa8ec6a101221c66e371542b84efd71dde09960
Johannes Schoenboeck <johannes.schoenboeck@fh-hagenberg.at>
2017-12-19 14:51:45 +0100

    files added

--- G2/Events/main.js
+++ G2/Events/main.js
@@ -4,11 +4,24 @@ window.onload=function () {
     let person = new Person("Hannes","Schönböck");
 
     let div = document.getElementById("eventDiv");
-    div.addEventListener("changeFirstName", function (e) {
+    /*div.addEventListener("changeFirstName", function (e) {
        console.log("Received change first name event");
        div.innerHTML = "<p>Changed first name form " + e.detail.old +
            " to " + e.detail.new;
-    });
+    });*/
+
+
+    var htmlHandler = function(oldVal, newVal){
+        console.log("Received change first name event");
+        console.log(this);
+        $(this).append("<p>Changed first name form " + oldVal +
+            " to " + newVal);
+    }
+
+    person.subscribe(div,htmlHandler);
 
     person.setFirstName("Johannes");
+
+    //person.addEventListener("changeFirstName", function(){})
+
 }
--- G2/Events/person.js
+++ G2/Events/person.js
@@ -2,6 +2,29 @@ export default class Person {
     constructor(fn,ln){
         this.firstName = fn;
         this.lastName = ln;
+        this.observers = [];
+    }
+
+    subscribe(observer,callbackFct){
+        this.observers.push({
+            obj:observer,
+            fct:callbackFct
+        });
+    }
+
+    unsubscribe(observer){
+        for(let i=0; i<this.observers.length;i++){
+            if(this.observers[i].obj == observer){
+                this.observers.splice(i,1);
+                break;
+            }
+        }
+    }
+
+    triggerEvent(oldVal,newVal){
+        for(let observer of this.observers){
+            observer.fct.call(observer.obj,oldVal,newVal);
+        }
     }
 
     print(){
@@ -13,10 +36,6 @@ export default class Person {
         let oldName = this.firstName;
         this.firstName = newFirstName;
 
-        //JS
-        let event = new CustomEvent("changeFirstName",
-            {"detail":{"old":oldName,"new":newFirstName}});
-        //feuern des Events
-        document.getElementById("eventDiv").dispatchEvent(event);
+        this.triggerEvent(oldName,newFirstName);
     }
 }
